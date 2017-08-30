ebf84b310cd8b1d636c7452492a532139d24db7c
NextIshwar <nextishwar@gmail.com>
2017-08-30 22:50:20 +0530

    site.master.cs

---
+++ Site.Master.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Library
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["admiId"] == null)
+            {
+                Response.Redirect("Home.aspx");
+            }
+        }
+        protected void Button10_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("Home.aspx");
+        }
+    }
+}
